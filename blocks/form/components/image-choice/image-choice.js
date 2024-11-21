182406caea3b2240cba114c64eb5baf9978ca55f
sakarora <sakarora@adobe.com>
2024-11-21 16:48:00 +0530

    Adding image choice

--- blocks/form/components/image-choice/image-choice.js
+++ blocks/form/components/image-choice/image-choice.js
@@ -1,9 +1,9 @@
 export default function decorate(fieldDiv, fieldJson) {
-    let radios = fieldDiv.querySelectorAll(".radio-wrapper");
+    const radios = fieldDiv.querySelectorAll('.radio-wrapper');
     const totalRadio = radios.length;
     for (let i = 0; i < totalRadio; i++) {
-        let label = radios[i].querySelector("label");
-        let img = document.createElement("img");
+        let label = radios[i].querySelector('label');
+        let img = document.createElement('img');
         img.src = fieldJson.properties.imageUrls[i];
         img.alt = fieldJson.enumNames[i];
         label.prepend(img);
